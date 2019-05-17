2931f9177f99876253cbc8e3eea18fcbe9147abd
alex <a.grigorenko.belvg@gmail.com>
2019-05-17 18:07:10 +0300

    OC3-10 [TASK] Added files with partial translations for Sweden localization

---
+++ admin/language/da-dk/extension/module/billmate_checkout.php
@@ -0,0 +1,36 @@
+<?php
+$_['heading_title'] = 'Billmate Checkout';
+
+// Text
+$_['text_extension']		= 'Extensions';
+$_['text_change_settings_success']		= 'Inställningarna uppdaterades framgångsrikt';
+$_['text_edit']			= 'Inställningar';
+// Entry
+$_['entry_billmate_status']		= 'Billmate Checkout Status';
+$_['entry_billmate_id']		= 'Billmate ID';
+$_['entry_billmate_secret']		= 'Billmate Secret';
+$_['entry_enable_bmc']			= 'Aktivera Billmate Checkout';
+$_['entry_enable_test_mode'] 	= 'Aktivera testläge';
+$_['entry_enable_push_events'] 	= 'Push orderhändelser till Billmate';
+$_['entry_default_order_status'] 	= 'Standard Order Status';
+$_['entry_link_terms_cond'] = 'Butiksvillkor';
+$_['entry_link_privacy_policy'] = 'Integritetspolicy';
+$_['entry_enable_logging_bm_requests'] = 'Aktivera loggningsbegäran (för utvecklare)';
+// Placeholder
+$_['placeholder_name'] 		= '';
+// Help
+$_['help_gdpr_message'] 		= 'Länk till sida som innehåller affärsvillkor';
+$_['help_privacy_policy_message'] 		= 'Länk till sida som innehåller butiksskyddspolicy';
+// Error
+$_['error_name'] 		= 'Modul ID tomt';
+//$_['error_message'] 		= 'Message field blank';
+$_['error_billmate_connection'] = 'Billmate-anslutningsfel : ';
+$_['error_age'] 		= 'Ålder måste vara numerisk';
+$_['error_redirect_url'] 	= 'URL kan inte vara tomt';
+
+$_['error_warning'] 		= 'Det finns problem med uppgifterna';
+$_['error_permission'] 		= 'Du har inte behörighet att redigera detta.';
+
+// Button
+$_['button_save'] 		= 'Spara formulär';
+$_['button_cancel'] 		= 'Annullera';
