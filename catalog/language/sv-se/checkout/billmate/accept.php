b728df53a5e9569e4f3b5410775366e670c36603
David Villa <david@kfoobar.se>
2021-03-30 13:17:14 +0200

    Move language files to new folder.

---
+++ catalog/language/sv-se/checkout/billmate/accept.php
@@ -0,0 +1,9 @@
+<?php
+
+// Errors
+$_['error_no_request']     = '[ACCEPT] Misslyckades med att hämta "request"-data';
+$_['error_no_connection']  = '[ACCEPT] Misslyckades med att etablera koppling till Billmate API.';
+$_['error_no_payment']     = '[ACCEPT] Misslyckades med att hämta "payment"-data.';
+$_['error_no_session']     = '[ACCEPT] Misslyckades med att hämta "session"-data.';
+$_['error_create_order']   = '[ACCEPT] Misslyckades med att hämta ordernummer.';
+$_['error_update_payment'] = '[ACCEPT] Misslyckades med att uppdatera betalningsstatus.';
