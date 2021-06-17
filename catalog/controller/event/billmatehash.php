951b41fef30e0b4a8410fcbf952aed4ec002d240
David Villa <david@kfoobar.se>
2021-06-17 14:36:08 +0200

    Remove unused code in events

--- catalog/controller/event/billmatehash.php
+++ catalog/controller/event/billmatehash.php
@@ -1,34 +1,14 @@
 <?php
-class ControllerEventBillmatehash extends Controller {
 
-    /**
-     * @var HelperBillmate
-     */
-    protected $helperBillmate;
-
-    /**
-     * ControllerEventBillmatecheckout constructor.
-     *
-     * @param $registry
-     */
+class ControllerEventBillmatehash extends Controller
+{
     public function __construct($registry)
     {
         parent::__construct($registry);
-        $this->helperBillmate  = new Helperbm($registry);
     }
 
-    /**
-     * @param $data
-     */
     public function validate(&$data)
     {
-        if (!$this->helperBillmate->isBmCheckoutEnabled()) {
-            return;
-        }
-        if(!$this->cart->hasProducts()) {
-            $this->helperBillmate->resetSessionBmHash();
-            $this->cart->clear();
-        }
-        $this->event->unregister('controller/*/before','event/billmatehash/validate');
+        return;
     }
 }
--- catalog/controller/event/billmateheader.php
+++ catalog/controller/event/billmateheader.php
@@ -1,79 +1,14 @@
 <?php
-class ControllerEventBillmateheader extends Controller {
 
-    /**
-     * @var array
-     */
-    protected $allowedJsRoutes = [
-        'checkout/checkout',
-        'checkout/checkout/index'
-    ];
-
-     /**
-     * @var array
-     */
-    protected $allowedCssRoutes = [
-        'checkout/checkout',
-        'checkout/checkout/index'
-    ];
-
-    /**
-     * @var HelperBillmate
-     */
-    protected $helperBillmate;
-
-    /**
-     * ControllerEventBillmatecheckout constructor.
-     *
-     * @param $registry
-     */
+class ControllerEventBillmateheader extends Controller
+{
     public function __construct($registry)
     {
         parent::__construct($registry);
-        $this->helperBillmate  = new Helperbm($registry);
     }
 
     public function addJsCss($route, $data)
     {
-        if(!$this->helperBillmate->isBmCheckoutEnabled()) {
-            return;
-        }
-
-        if ($this->isAllowedToShowJs()) {
-            $this->document->addScript('catalog/view/javascript/billmate/bm-checkout.js');
-        }
-
-        if ($this->isAllowedToShowCss()) {
-            $this->document->addStyle('catalog/view/theme/default/stylesheet/billmate/bm-checkout.css');
-        }
-    }
-
-    /**
-     * @return bool
-     */
-    protected function isAllowedToShowJs()
-    {
-        $requestRoute = $this->getRequestRoute();
-        return in_array($requestRoute, $this->allowedJsRoutes);
-    }
-
-    /**
-     * @return bool
-     */
-    protected function isAllowedToShowCss()
-    {
-        $requestRoute = $this->getRequestRoute();
-        return in_array($requestRoute, $this->allowedCssRoutes);
-    }
-
-    /**
-     * @return string
-     */
-    protected function getRequestRoute()
-    {
-        if (isset($this->request->get['route'])) {
-            return $this->request->get['route'];
-        }
-        return '';
+        return;
     }
 }
