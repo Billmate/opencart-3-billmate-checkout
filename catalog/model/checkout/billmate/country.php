e967383980ad7f0ddccc658ee62528b6d736fde2
David Villa <david@kfoobar.se>
2021-06-17 14:31:43 +0200

    Remove doc block. Fix indent.

--- catalog/model/checkout/billmate/country.php
+++ catalog/model/checkout/billmate/country.php
@@ -4,8 +4,8 @@ class ModelCheckoutBillmateCountry extends Model
 {
     public function getCountryIdByCode($country_code)
     {
-       $query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "country WHERE iso_code_2 = '" . $this->db->escape($country_code) . "'");
+        $query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "country WHERE iso_code_2 = '" . $this->db->escape($country_code) . "'");
 
-       return !empty($query->row) ? $query->row['country_id'] : 0;
+        return !empty($query->row) ? $query->row['country_id'] : 0;
     }
 }
--- catalog/model/extension/payment/billmate_checkout.php
+++ catalog/model/extension/payment/billmate_checkout.php
@@ -2,12 +2,6 @@
 
 class ModelExtensionPaymentBillmateCheckout extends Model
 {
-    /**
-     * @param $address
-     * @param $total
-     *
-     * @return array
-     */
     public function getMethod($address, $total)
     {
         return [];
